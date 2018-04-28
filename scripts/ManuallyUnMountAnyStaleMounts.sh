2972e3f79e0e7c97ae4dd8b48c17e21de4d4d11d
SuperKMG <4595401+kelsieflynn@users.noreply.github.com>
2018-04-27 23:46:23 -0400

    Update ManuallyUnMountAnyStaleMounts.sh

--- scripts/ManuallyUnMountAnyStaleMounts.sh
+++ scripts/ManuallyUnMountAnyStaleMounts.sh
@@ -1,17 +1,29 @@
 #!/bin/bash -e
 if [ `basename $PWD`  =  "scripts" ];
 	then 
-		sudo umount -l ../work/*/stage[01234]/rootfs/dev/pts
-		sudo umount -l ../work/*/stage[01234]/rootfs/dev
-		sudo umount -l ../work/*/stage[01234]/rootfs/sys
-		sudo umount -l ../work/*/stage[01234]/rootfs/proc
+		sudo umount -l ../work/*/stage[0..4]/rootfs/dev/pts
+		sudo umount -l ../work/*/stage[0..4]/rootfs/dev
+		sudo umount -l ../work/*/stage[0..4]/rootfs/sys
+		sudo umount -l ../work/*/stage[0..4]/rootfs/proc
+		sudo umount -l ../work/*/export-image/rootfs/dev/pts
+		sudo umount -l ../work/*/export-image/rootfs/dev
+		sudo umount -l ../work/*/export-image/rootfs/sys
+		sudo umount -l ../work/*/export-image/rootfs/proc
+		sudo umount -l ../work/*/export-image/rootfs/boot
+		sudo umount -l ../work/*/export-image/rootfs
 		#mount -l work/*/stage[012]/rootfs/{dev,sys,proc,dev/pts}
 	
 else
 
-		sudo umount -l ./work/*/stage[01234]/rootfs/dev/pts
-		sudo umount -l ./work/*/stage[01234]/rootfs/dev
-		sudo umount -l ./work/*/stage[01234]/rootfs/sys
-		sudo umount -l ./work/*/stage[01234]/rootfs/proc
+		sudo umount -l ./work/*/stage[0..4]/rootfs/dev/pts
+		sudo umount -l ./work/*/stage[0..4]/rootfs/dev
+		sudo umount -l ./work/*/stage[0..4]/rootfs/sys
+		sudo umount -l ./work/*/stage[0..4]/rootfs/proc
+		sudo umount -l ./work/*/export-image/rootfs/dev/pts
+		sudo umount -l ./work/*/export-image/rootfs/dev
+		sudo umount -l ./work/*/export-image/rootfs/sys
+		sudo umount -l ./work/*/export-image/rootfs/proc
+		sudo umount -l ./work/*/export-image/rootfs/boot
+		sudo umount -l ./work/*/export-image/rootfs
 		#mount -l work/*/stage[012]/rootfs/{dev,sys,proc,dev/pts}
 fi
